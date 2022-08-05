2c51cf562384e4c21b926e5b158e8f9c133549a5
TheCodeHeist <riyasat.hossain.29@gmail.com>
2022-08-05 15:17:36 +0600

    New Post Editing Feature Added...

--- src/routes/feed.svelte
+++ src/routes/feed.svelte
@@ -1,10 +1,81 @@
 <script lang="ts">
+  import { scale } from "svelte/transition";
   import Post from "../lib/post.svelte";
+
+  let showEditor = true;
+  let newPostContent = "";
 </script>
 
 <svelte:head>Showing Posts</svelte:head>
 
-<div class="bg-slate-900 p-10">
+<div class="w-full bg-slate-900 flex flex-col justify-start items-center py-10">
+  <div
+    class="w-11/12 min-h-[5rem] flex justify-center m-10 py-4 px-6 rounded-xl bg-slate-800 shadow-xl flex-shrink-0"
+    in:scale={{ duration: 300 }}
+  >
+    <div
+      class="w-14 h-14 border border-slate-600 rounded-full overflow-hidden flex justify-center items-center mr-6 flex-shrink-0"
+    >
+      <img
+        src="https://www.gravatar.com/avatar/{'Ashraf'.length}?s=47&d=robohash"
+        alt={"current user"}
+        class="object-cover w-full h-full"
+      />
+    </div>
+
+    <button
+      type="button"
+      class={"w-10/12 h-14 border border-slate-700 rounded-full text-left px-8 cursor-text overflow-hidden flex-shrink-0 whitespace-nowrap" +
+        (newPostContent ? "  text-white" : " text-gray-400")}
+      on:click={() => {
+        showEditor = true;
+      }}
+    >
+      {newPostContent ? newPostContent : "What's in you mind, Ashraf?"}</button
+    >
+  </div>
+
+  {#if showEditor}
+    <div
+      class="fixed h-full w-full bg-[#0005] flex justify-center items-center"
+    >
+      <div
+        class="h-4/6 w-7/12 bg-slate-800 rounded-2xl flex flex-col shadow-xl border border-slate-600 overflow-hidden"
+        in:scale={{ duration: 300 }}
+      >
+        <div
+          class="w-full h-1/6 flex items-center justify-between px-10 border-b border-slate-600"
+        >
+          <p class="font-bold text-3xl">Create Post</p>
+
+          <button
+            type="button"
+            class="flex justify-center items-center px-4 py-2"
+            on:click={() => {
+              showEditor = false;
+            }}><p class="font-bold text-3xl h-full w-full">&times;</p></button
+          >
+        </div>
+
+        <textarea
+          class="h-4/6 w-full px-6 py-8 resize-none bg-slate-700 focus:outline-none"
+          placeholder="What's in you mind, Ashraf?"
+          bind:value={newPostContent}
+        />
+
+        <div
+          class="w-full h-1/6 flex items-center justify-center border-t border-slate-600"
+        >
+          <button
+            type="button"
+            class="h-4/6 w-11/12 resize-none bg-slate-900 rounded-lg font-semibold text-xl transition-all outline-none hover:grayscale-[40%]"
+            >Send</button
+          >
+        </div>
+      </div>
+    </div>
+  {/if}
+
   <Post poster={"Siam"} post={"what the hell this is?"} />
   <Post poster={"Ashraf"} post={"what am I talking about right now?"} />
   <Post poster={"Ashraf"} post={"what am I talking about right now?"} />
