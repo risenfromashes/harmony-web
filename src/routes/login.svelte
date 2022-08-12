72bedcf994e8fecfeab6b0c29b4b4ffd45671e9d
Sadat-Hossain-01 <sadatho2014@gmail.com>
2022-08-12 08:36:55 +0600

    Update login.svelte

--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -69,7 +69,7 @@
           type="password"
           bind:value={password}
           placeholder="Password"
-          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mt-5 font-Roboto"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mt-5 font-OpenSans"
         />
       </div>
       <div class="mx-auto py-8">
