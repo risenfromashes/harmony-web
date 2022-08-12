c0f1fdbe61298a53949f769a3b09aa85bc392ac7
TheCodeHeist <riyasat.hossain.29@gmail.com>
2022-08-13 00:53:42 +0600

    Chatbox added in Chats page...

--- src/lib/chatchannelviewer.svelte
+++ src/lib/chatchannelviewer.svelte
@@ -7,22 +7,31 @@
   $: query = new URLSearchParams($location.search);
 </script>
 
-<div class="h-full w-10/12 px-10 py-8">
+<div class="h-full w-10/12">
   {#if $location.search.length > 0}
-    <h1 class="text-5xl font-bold mb-12">Chats</h1>
+    <!-- Chat Viewer -->
+    <div class="h-5/6 w-full px-10 py-8 border-b border-slate-700" />
 
-    <p class="text-xl">
-      <strong>Group ID: </strong>
-      {query.get("g")}
-    </p>
+    <!-- Chatbox -->
+    <div class="h-1/6 w-full flex justify-center items-center">
+      <textarea
+        class="resize-none w-5/6 max-h-[4rem] overflow-y-scroll bg-slate-800 px-4 py-3 rounded-lg mr-4 overflow-x-hidden"
+        placeholder="Type your message here... (Markdown supported)"
+      />
 
-    <p class="text-xl">
-      <strong>Channel ID: </strong>
-      {query.get("c")}
-    </p>
+      <button
+        type="button"
+        class="flex bg-slate-800 px-6 py-4 rounded-lg transition-all hover:bg-slate-700"
+      >
+        <FaIcon icon="paper-plane" />
+        <p class="ml-3">Send</p>
+      </button>
+    </div>
   {:else}
     <div class="h-full w-full flex flex-col items-center justify-center">
-      <p class="text-2xl text-slate-700">Select a group and channel to view the chat.</p>
+      <p class="text-2xl text-slate-700">
+        Select a group and channel to view the chat.
+      </p>
       <FaIcon icon="bolt" className="text-[20rem] text-slate-800" />
     </div>
   {/if}
