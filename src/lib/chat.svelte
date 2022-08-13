5d9d50bca2177cdef433c02810e107838ba2e885
TheCodeHeist <riyasat.hossain.29@gmail.com>
2022-08-13 18:29:10 +0600

    Chat Viewer and Chat Editor (Quill) added in Chats page, Quill editorcodeblock styles patched...

---
+++ src/lib/chat.svelte
@@ -0,0 +1,44 @@
+<script>
+  import { scale } from "svelte/transition";
+  export let poster = "";
+  export let time = ""; //when it was posted
+  export let post = "";
+  // let showComments = false;
+
+  // $: showCommentStatement = showComments ? "Hide Comments" : "Show Comments";
+</script>
+
+<div
+  class="w-full min-h-[5rem] flex flex-col my-2 py-3 px-6 border-y border-slate-800 flex-shrink-0"
+  in:scale={{ duration: 300 }}
+>
+  <div class="flex mb-4">
+    <div
+      class="w-14 h-14 border border-slate-600 rounded-full overflow-hidden flex justify-center items-center mr-4 flex-shrink-0"
+    >
+      <img
+        src="https://www.gravatar.com/avatar/{poster.length}?s=47&d=robohash"
+        alt={poster}
+        class="object-cover w-full h-full"
+      />
+    </div>
+
+    <div class="p-2">
+      <h5 class="font-semibold text-lg mb-2">
+        {poster}
+        <span class="ml-2 font-semibold text-sm text-slate-600">{time}</span>
+      </h5>
+      <div class="flex flex-col flex-1">
+        <p>{post}</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="w-full flex justify-between">
+    <!-- <p class="card__comments">
+      {showCommentStatement}
+    </p> -->
+
+    <p class="reactions" />
+  </div>
+</div>
--- src/lib/chatchannelviewer.svelte
+++ src/lib/chatchannelviewer.svelte
@@ -1,32 +1,114 @@
 <script lang="ts">
   import { useLocation } from "svelte-navigator";
+  import { scale } from "svelte/transition";
+  import Chat from "./chat.svelte";
   import FaIcon from "./faIcon.svelte";
+  import Quill from "./quill.svelte";
 
   const location = useLocation();
 
   $: query = new URLSearchParams($location.search);
+
+  let showEditor = false;
+
+  let newPostContent = {
+    delta: {},
+    text: "",
+  };
+
+  let onTextChange = (e: any) => {
+    console.log(e.detail);
+    if (showEditor) {
+      newPostContent = e.detail;
+    }
+  };
 </script>
 
-<div class="h-full w-10/12">
+<div class="h-full w-10/12 relative">
   {#if $location.search.length > 0}
-    <!-- Chat Viewer -->
-    <div class="h-5/6 w-full px-10 py-8 border-b border-slate-700" />
-
-    <!-- Chatbox -->
-    <div class="h-1/6 w-full flex justify-center items-center">
-      <textarea
-        class="resize-none w-5/6 max-h-[4rem] overflow-y-scroll bg-slate-800 px-4 py-3 rounded-lg mr-4 overflow-x-hidden"
-        placeholder="Type your message here... (Markdown supported)"
-      />
-
-      <button
-        type="button"
-        class="flex bg-slate-800 px-6 py-4 rounded-lg transition-all hover:bg-slate-700"
+    <!-- Chat Writer -->
+    {#if showEditor}
+      <div
+        class="absolute h-full w-full bg-[#0005] flex justify-center items-center z-20"
       >
-        <FaIcon icon="paper-plane" />
-        <p class="ml-3">Send</p>
-      </button>
+        <div
+          class="h-5/6 w-7/12 rounded-2xl flex flex-col shadow-xl border bg-slate-800 border-slate-600 overflow-hidden"
+          in:scale={{ duration: 300 }}
+        >
+          <div
+            class="w-full h-1/6 flex items-center justify-between px-10 bg-slate-800 border-b border-slate-600 relative"
+          >
+            <p class="font-bold text-3xl mr-4">
+              <FaIcon icon="file-pen" />&nbsp;&nbsp;Create Post
+            </p>
+
+            <button
+              type="button"
+              class="flex justify-center items-center mx-4 my-2 hover:text-rose-400"
+              on:click={() => {
+                showEditor = false;
+              }}
+              ><p class="font-bold text-3xl h-full w-full transition-all">
+                <FaIcon icon="times" />
+              </p></button
+            >
+          </div>
+
+          <!-- <textarea
+        class="h-4/6 w-full px-6 py-8 resize-none bg-slate-700 focus:outline-none"
+        placeholder="What's in you mind, Ashraf?"
+        bind:value={newPostContent}
+      /> -->
+
+          <Quill className="h-4/6 w-full" on:textChange={onTextChange} />
+
+          <div
+            class="w-full h-1/6 flex items-center justify-center border-t border-slate-600 bg-slate-800"
+          >
+            <button
+              type="button"
+              class="resize-none rounded-lg font-semibold text-xl transition-all outline-none hover:text-emerald-400"
+              ><FaIcon icon="paper-plane" />&nbsp;&nbsp;Send</button
+            >
+          </div>
+        </div>
+      </div>
+    {/if}
+
+    <!-- Chat Viewer -->
+    <div
+      class="h-full w-full px-10 py-8 flex flex-col items-center overflow-y-scroll overflow-x-hidden"
+    >
+      <Chat poster="Ashraf" time={Date()} post={`sadasfsdfasdf`} />
+      <Chat poster="Siam" time={Date()} post={`sadasfsdfasdf`} />
+      <Chat poster="Sadat" time={Date()} post={`sadasfsdfasdf`} />
+      <Chat poster="Ashraf" time={Date()} post={`sadasfsdfasdf`} />
+      <Chat poster="Siam" time={Date()} post={`sadasfsdfasdf`} />
+      <Chat poster="Sadat" time={Date()} post={`sadasfsdfasdf`} />
+      <Chat poster="Ashraf" time={Date()} post={`sadasfsdfasdf`} />
+      <Chat poster="Siam" time={Date()} post={`sadasfsdfasdf`} />
+      <Chat poster="Sadat" time={Date()} post={`sadasfsdfasdf`} />
+      <Chat poster="Ashraf" time={Date()} post={`sadasfsdfasdf`} />
+      <Chat poster="Siam" time={Date()} post={`sadasfsdfasdf`} />
+      <Chat poster="Sadat" time={Date()} post={`sadasfsdfasdf`} />
+      <Chat poster="Ashraf" time={Date()} post={`sadasfsdfasdf`} />
+      <Chat poster="Siam" time={Date()} post={`sadasfsdfasdf`} />
+      <Chat poster="Sadat" time={Date()} post={`sadasfsdfasdf`} />
+      <Chat poster="Ashraf" time={Date()} post={`sadasfsdfasdf`} />
+      <Chat poster="Siam" time={Date()} post={`sadasfsdfasdf`} />
+      <Chat poster="Sadat" time={Date()} post={`sadasfsdfasdf`} />
     </div>
+
+    <!-- Floating Button -->
+    <button
+      type="button"
+      class="absolute bottom-20 right-20 bg-blue-600 w-14 h-14 rounded-full shadow-lg text-white flex items-center justify-center transition-all hover:bg-blue-700"
+      on:click={() => {
+        showEditor = true;
+      }}
+    >
+      <FaIcon icon="comment-dots" className="text-white text-2xl" />
+    </button>
   {:else}
     <div class="h-full w-full flex flex-col items-center justify-center">
       <p class="text-2xl text-slate-700">
