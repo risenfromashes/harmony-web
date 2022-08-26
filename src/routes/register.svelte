3279141b5b669b5ac074f2ae4aa7291542345688
Sadat-Hossain-01 <sadatho2014@gmail.com>
2022-08-26 20:54:52 +0600

    added fancy password authentication

--- src/routes/register.svelte
+++ src/routes/register.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
   let reg = {
     user_name: "",
     password: "",
@@ -24,7 +24,7 @@
   let warning_text = "";
 
   const submit = async () => {
-    if (!matched) {
+    if (!matched || reg.password.length < 5) {
       return;
     }
 
@@ -59,6 +59,29 @@
       warning_text = `Error: ${e}`;
     }
   };
+
+  let bothcase: boolean = false;
+  let symbol: boolean = false;
+  let largepass: boolean = false;
+  let level: number = 0;
+  const specialChars = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+  $: {
+    largepass = reg.password.length >= 10 ? true : false;
+    //check if password contains letters of both cases
+    bothcase =
+      reg.password.match(/[a-z]/) && reg.password.match(/[A-Z]/) ? true : false;
+    //check if the password contains any symbol
+    symbol = specialChars.test(reg.password) ? true : false;
+    if (reg.password.length >= 5) {
+      level = 1;
+      if (bothcase) level++;
+      if (largepass) level++;
+      if (symbol) level++;
+    } else level = 0;
+    console.log(level, symbol, bothcase, largepass);
+  }
+
+  let showtooltip: boolean = false;
 </script>
 
 <div class="bg-slate-900">
@@ -160,6 +183,7 @@
         class="block mb-2 font-OpenSans text-sm font-medium text-gray-900 dark:text-gray-300"
         >Password</label
       >
+      <!-- svelte-ignore a11y-mouse-events-have-key-events -->
       <input
         type="password"
         id="password"
@@ -167,7 +191,123 @@
         placeholder="•••••••••"
         bind:value={reg.password}
         required
+        on:mouseover={() => (showtooltip = true)}
+        on:mouseout={() => (showtooltip = false)}
       />
+      {#if showtooltip}
+        <div
+          id="popover-password"
+          role="tooltip"
+          class="inline-block absolute z-10 w-72 text-sm font-light text-gray-500 bg-white rounded-lg border border-gray-200 shadow-sm transition-opacity duration-300 dark:bg-gray-800 dark:border-gray-600 dark:text-gray-400"
+        >
+          <div class="p-3 space-y-2">
+            <h3 class="font-semibold text-gray-900 dark:text-white">
+              Must have at least 5 characters
+            </h3>
+            <div class="grid grid-cols-4 gap-2">
+              {#each { length: level } as _, i}
+                <div class="h-1 bg-orange-300 dark:bg-orange-400" />
+              {/each}
+              {#each { length: 4 - level } as _, i}
+                <div class="h-1 bg-gray-200 dark:bg-gray-600" />
+              {/each}
+            </div>
+            <p>It’s better to have:</p>
+            <ul>
+              <li class="flex items-center mb-1">
+                {#if bothcase}
+                  <svg
+                    class="mr-2 w-4 h-4 text-green-400 dark:text-green-500"
+                    aria-hidden="true"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                    ><path
+                      fill-rule="evenodd"
+                      d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                      clip-rule="evenodd"
+                    /></svg
+                  >
+                {:else}
+                  <svg
+                    class="mr-2 w-4 h-4 text-gray-300 dark:text-gray-400"
+                    aria-hidden="true"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                    ><path
+                      fill-rule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clip-rule="evenodd"
+                    /></svg
+                  >
+                {/if}
+                Upper & lower case letters
+              </li>
+              <li class="flex items-center mb-1">
+                {#if symbol}
+                  <svg
+                    class="mr-2 w-4 h-4 text-green-400 dark:text-green-500"
+                    aria-hidden="true"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                    ><path
+                      fill-rule="evenodd"
+                      d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                      clip-rule="evenodd"
+                    /></svg
+                  >
+                {:else}
+                  <svg
+                    class="mr-2 w-4 h-4 text-gray-300 dark:text-gray-400"
+                    aria-hidden="true"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                    ><path
+                      fill-rule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clip-rule="evenodd"
+                    /></svg
+                  >
+                {/if}
+                A symbol (#$&)
+              </li>
+              <li class="flex items-center">
+                {#if largepass}
+                  <svg
+                    class="mr-2 w-4 h-4 text-green-400 dark:text-green-500"
+                    aria-hidden="true"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                    ><path
+                      fill-rule="evenodd"
+                      d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                      clip-rule="evenodd"
+                    /></svg
+                  >
+                {:else}
+                  <svg
+                    class="mr-2 w-4 h-4 text-gray-300 dark:text-gray-400"
+                    aria-hidden="true"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                    ><path
+                      fill-rule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clip-rule="evenodd"
+                    /></svg
+                  >
+                {/if}
+                A longer password (min. 10 chars.)
+              </li>
+            </ul>
+          </div>
+        </div>
+      {/if}
     </div>
     <div class="mb-6">
       <label
