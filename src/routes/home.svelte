c574358038cebd8a03e00449fdb885bb0493c709
Sadat-Hossain-01 <sadatho2014@gmail.com>
2022-08-27 20:04:30 +0600

    minor change

--- src/routes/home.svelte
+++ src/routes/home.svelte
@@ -21,6 +21,8 @@
     <div class="pt-20 w-full h-screen flex overflow-hidden">
       <GroupTree groups={$groups} />
       <Tabs />
+      <!-- showing the feed page by default, still recheck this later, like the group and subject choices -->
+      <Feed />
     </div>
   </Route>
   <Route path="/feed">
