ceb18476c6210a9d14e1effbe59212f9e71f7a54
Sadat-Hossain-01 <sadatho2014@gmail.com>
2022-08-31 03:59:51 +0600

    changed edit event button

--- src/lib/eventitem.svelte
+++ src/lib/eventitem.svelte
@@ -41,10 +41,15 @@
       >
     {/if}
     <button
+      class="h-10 w-10 absolute right-5 shadow-xl text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-base dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
       type="button"
-      class="absolute right-5 text-white bg-blue-700 dark:bg-blue-600 hover:bg-blue-800 dark:hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 text-center w-1/10"
-      on:click|stopPropagation={() => (draweropen = true)}>Edit</button
+      data-drawer-target="drawer-form"
+      data-drawer-show="drawer-form"
+      aria-controls="drawer-form"
+      on:click|stopPropagation={() => (draweropen = true)}
     >
+      <FaIcon type="regular" icon="pencil" className="text-base" />
+    </button>
   </h3>
   <time
     class="block mb-2 text-base leading-none text-gray-400 dark:text-gray-500"
